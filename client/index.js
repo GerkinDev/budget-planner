d53e9e691159905049d4d0f2baff2a4a5d6c609f
Alexandre Germain <alexandre.germain@craft.ai>
2023-10-08 00:53:56 +0200

    chore(client): setup react-native-paper-{dates,dropdown}

--- client/index.js
+++ client/index.js
@@ -1,12 +1,62 @@
 /**
- * @format
- */
+* @format
+*/
 
 import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import { registerRootComponent } from 'expo';
 import App from './src/App';
 import {name as appName} from './app.json';
+
+//#region https://web-ridge.github.io/react-native-paper-dates/docs/intro#prerequisites
+// on top of your index.android.js file
+const isAndroid = require('react-native').Platform.OS === 'android' // this line is only needed if you don't use an .android.js file
+const isHermesEnabled = !!global.HermesInternal // this line is only needed if you don't use an .android.js file
+
+// in your index.js file
+if (isHermesEnabled || isAndroid) {
+	// this line is only needed if you don't use an .android.js file
+	
+	require('@formatjs/intl-getcanonicallocales/polyfill')
+	require('@formatjs/intl-locale/polyfill')
+	
+	require('@formatjs/intl-pluralrules/polyfill')
+	require('@formatjs/intl-pluralrules/locale-data/en.js') // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+	
+	require('@formatjs/intl-displaynames/polyfill')
+	require('@formatjs/intl-displaynames/locale-data/en.js') // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+	
+	require('@formatjs/intl-listformat/polyfill')
+	require('@formatjs/intl-listformat/locale-data/en.js') // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+	
+	require('@formatjs/intl-numberformat/polyfill')
+	require('@formatjs/intl-numberformat/locale-data/en.js') // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+	
+	require('@formatjs/intl-relativetimeformat/polyfill')
+	require('@formatjs/intl-relativetimeformat/locale-data/en.js') // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+	
+	require('@formatjs/intl-datetimeformat/polyfill')
+	require('@formatjs/intl-datetimeformat/locale-data/en.js') // USE YOUR OWN LANGUAGE OR MULTIPLE IMPORTS YOU WANT TO SUPPORT
+	
+	require('@formatjs/intl-datetimeformat/add-golden-tz.js')
+	
+	// https://formatjs.io/docs/polyfills/intl-datetimeformat/#default-timezone
+	if ('__setDefaultTimeZone' in Intl.DateTimeFormat) {
+		// // If you are using react-native-cli
+		// let RNLocalize = require('react-native-localize')
+		// Intl.DateTimeFormat.__setDefaultTimeZone(RNLocalize.getTimeZone())
+		
+		//  Are you using Expo, use this instead of previous 2 lines
+		Intl.DateTimeFormat.__setDefaultTimeZone(
+			require("expo-localization").timezone
+		);
+	}
+} // this line is only needed if you don't use an .android.js file
+
+import { en, registerTranslation } from 'react-native-paper-dates/lib/module'
+registerTranslation('en', en)
+
+//#endregion
 
 AppRegistry.registerComponent(appName, () => App);
 registerRootComponent(App)
---
+++ client/src/types.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native-paper-dates/lib/module/Date/DatePickerModal' {
+  export * from 'react-native-paper-dates/lib/typescript/Date/DatePickerModal';
+}
+declare module 'react-native-paper-dates/lib/module' {
+  export * from 'react-native-paper-dates/lib/typescript';
+}
